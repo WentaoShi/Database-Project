c88b637878a16cf87b08f137c54f3c541983c095
WentaoShi <wentao.shi@nyu.edu>
2016-04-18 23:38:04 -0400

    Progress update.

---
+++ storePicToTmp.php
@@ -0,0 +1,15 @@
+<?php
+  $res = pg_query("SELECT user_photo FROM users WHERE username='{$uname}'");  
+  $image = stripcslashes(pg_fetch_result($res,0,0));
+
+  $data = pg_fetch_result($res, 'user_photo');
+  $unes_image = pg_unescape_bytea($data);
+
+  $file_name = "profile_pic/{$uname}_profile.jpg";
+  $img = fopen($file_name, 'wb');
+  fwrite($img, $unes_image);
+  fclose($img);
+
+  pg_close($conn); 
+
+?>
