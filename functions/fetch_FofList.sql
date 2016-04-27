c797470ca648a84af6c92c164cfae478ff973bff
WentaoShi <wentao.shi@nyu.edu>
2016-04-27 17:41:03 -0400

    Bug fixed

--- functions/fetch_FofList.sql
+++ functions/fetch_FofList.sql
@@ -11,7 +11,14 @@ begin
 	union
 	select username2 as fof from friend
 	where username in (select * from FetchFriendNameList(me)) and username2 != me
+	union
+
+	select DISTINCT username2 as direct_friend from friend where username=me and status = 'accepted'
+	union
+	select DISTINCT username as direct_friend from friend where username2=me and status = 'accepted'
+
 	) as tmp;
+
 end;
 $$ language plpgsql;
 
